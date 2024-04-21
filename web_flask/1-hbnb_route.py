92bb612c56f1ad16eff7c45d00f0c98b4622fd90
peterlubega <peterlubega100@gmail.com>
2024-04-21 11:11:48 +0000

    fix: fixing task 2 to meet the style format

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 Script that starts a Flask web application
 """
 
@@ -12,10 +12,12 @@ def hello():
     """Returns the Hello HBNB"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Returns HBNB!"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
